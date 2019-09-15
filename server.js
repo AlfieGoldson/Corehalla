0e19d7cc5b7790af6549f9ac6609243f5798ad02
Alfie <corehallabusiness@gmail.com>
2019-09-15 14:02:35 +0200

    spa express routes

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/'));
 
+app.get(/.*/, (req, res) => {
+    res.sendfile(__dirname + '/dist/index.html');
+})
+
 app.listen(PORT, () => {
     console.log(`Server Listening on Port ${PORT}`);
 })
