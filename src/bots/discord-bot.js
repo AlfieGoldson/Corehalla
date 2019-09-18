6e5ebeeac465e608a4b1677a1795d67956f16334
AlfieGoldson <mrxblack361@gmail.com>
2019-09-18 07:55:48 +0000

    leadEmbedTest

--- src/bots/discord-bot.js
+++ src/bots/discord-bot.js
@@ -9,10 +9,19 @@ client.on('ready', () => {
 
 client.on('message', msg => {
     const args = msg.content.split(' ');
+    const userID = msg.author.id;
     if (args[0].startsWith('!')) {
         switch(args[0]) {
             case '!stats':
             msg.channel.send('Your Stats');
+            break;
+
+            case '!rank':
+            msg.channel.send('Your Stats');
+            break;
+
+            case '!lead':
+            msg.channel.send(generateLeadEmbed(args[1] || '1v1', args[2] || 'all', args[3] || '1'));
             break;
 
             default:
@@ -27,3 +36,9 @@ client.on('message', msg => {
 client.on('error', console.error);
 
 client.login(process.env.DISCORD_BOT_TOKEN);
+
+const generateLeadEmbed = (bracket, region, page) => {
+    bh_api.fetchLeaderboard({ bracket, region, page }).then(data => {
+
+    })
+}
