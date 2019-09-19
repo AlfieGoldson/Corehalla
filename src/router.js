60762a8d2633fcf8b3281ca80d4889ba7b48b2d6
Djobbo Maïga <corehallabusiness@gmail.com>
2019-09-19 08:45:01 +0000

    document format cleanup

--- src/router.js
+++ src/router.js
@@ -8,28 +8,28 @@ import Legend from './views/wiki/legends/legend.vue'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/stats/leaderboard/:bracket?/:region?/:page?',
-      name: 'leaderboard',
-      component: Leaderboard
-    },
-    {
-      path: '/stats/player/:id',
-      name: 'player-stats',
-      component: PlayerStats
-    },
-    {
-      path: '/l',
-      name: 'wiki-legend-page',
-      component: Legend
-    }
-  ]
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/stats/leaderboard/:bracket?/:region?/:page?',
+            name: 'leaderboard',
+            component: Leaderboard
+        },
+        {
+            path: '/stats/player/:id',
+            name: 'player-stats',
+            component: PlayerStats
+        },
+        {
+            path: '/l',
+            name: 'wiki-legend-page',
+            component: Legend
+        }
+    ]
 })
